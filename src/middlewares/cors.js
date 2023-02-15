bfade41fe8202479466bfa968069f5b07c42a1ad
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-02-15 10:44:35 +0200

    update CORS middleware

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
-    res.setHeader('Access-Control-Origin', '*');
-    res.setHeader('Access-Control-Methods', 'GET', 'POST', 'PUT', 'DELETE');
-    res.setHeader('Access-Control-Headers', 'Content-Type, Authorization');
-
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 }
