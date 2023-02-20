0bbc34e5a29964ae1d3dbfe8a8af2f374ca5d333
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-02-20 08:42:21 +0200

    minor change

--- middlewares/session.js
+++ middlewares/session.js
@@ -3,14 +3,14 @@ const { parseToken } = require("../services/userService.js");
 module.exports = () => (req, res, next) => {
     const token = req.headers['x-authorization'];
 
-    try {
-        if (token) {
+    if (token) {
+        try {
             const payload = parseToken(token)
             req.user = payload;
             req.token = token;
+        } catch (error) {
+            return res.status(404).json({ message: 'Invalid Session Token' })
         }
-    } catch (error) {
-        res.status(404).json({ message: 'Invalid Session Token' })
     }
     next();
 }
